b2c1d0ecc4a5585d7b407ff0a4229dcb7d79a9e1
Marat Nurgaliev <iknow.whoiam@mail.ru>
2019-10-01 16:52:56 +0300

    Из класса VkFeed убрал функции в пресентер, сделав его "чистым"

--- homeWork_1/model/vk/VkFeed.swift
+++ homeWork_1/model/vk/VkFeed.swift
@@ -29,44 +29,6 @@ class VkFeed {
     
     var isLiked = false
     
-    func getFeedDate() -> String {
-        
-        let currentDate = Date().timeIntervalSince1970
-        
-        let diffInSeconds = currentDate - Double(feedDate)
-        let diffInMinutes = diffInSeconds/60
-        let diffInHours = diffInMinutes/60
-        let diffInDays = diffInMinutes/24
-        
-        if (diffInDays < 1
-            && diffInHours < 1
-            && diffInMinutes < 1
-            && diffInSeconds < 60) {
-            return "\(Int(diffInSeconds)) секунд назад"
-        } else if (diffInDays < 1
-            && diffInHours < 1
-            && diffInMinutes < 60) {
-            return "\(Int(diffInMinutes)) минут назад"
-        } else if (diffInDays < 1
-            && diffInHours < 24) {
-            return "\(Int(diffInHours)) часов назад"
-        } else {
-            return "\(Int(diffInDays)) дней назад"
-        }
-    }
-    
-    func getStringFrom(count: Int) -> String {
-        if count > 1000000 {
-            return String(format: "%.2f М ", Float(count)/1000000)
-        } else if count > 1000 {
-            return String(format: "%.2f К ", Float(count)/1000)
-        } else if count > 0 {
-            return "\(count)"
-        } else {
-            return ""
-        }
-    }
-    
 }
 
 class VkAttachment { //only photo
